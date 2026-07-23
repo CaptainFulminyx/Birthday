ac11e3b2e4059830fe31285bfb66ad8cd24bf60b
CaptainFulminyx <captainfulminyx@gmail.com>
2026-07-24 01:17:52 +0530

    Init 0.0

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+<template>
+  <div>
+    <Icon
+      icon="fluent:bow-tie-24-regular"
+      width="240px"
+      height="240px"
+      color="#f00"
+    />
+  </div>
+</template>
